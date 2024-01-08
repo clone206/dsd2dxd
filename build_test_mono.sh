a0494cf0879a24fa70a6c27bd9424cd9ff484cb3
clone206 <clone206@example.com>
2024-01-07 21:46:34 -0800

    Simplified build test scripts to pipe to ffplay directly

--- build_test_mono.sh
+++ build_test_mono.sh
@@ -8,7 +8,4 @@ then
 fi
 
 g++ *.c *.cpp -O3 -o dsd2pcm
-./dsd2pcm 1 $1 $2 $3 < $4 > out.pcm
-ffmpeg -y -f s${outbits}le -ar 352.8k -ac 1 -i out.pcm -c:a pcm_s${outbits}le mono.wav
-rm -f out.pcm
-ffplay mono.wav
+./dsd2pcm 1 $1 $2 $3 < $4 | ffplay -f s${outbits}le -ar 352.8k -ac 1 -i -
