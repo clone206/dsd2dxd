2e40594344c73eeaaf6c9874665d355abcb5c9bd
clone206 <clone206@example.com>
2024-06-29 14:41:05 -0700

    Test scripts using -O2 optimization level

--- build_test_stereo.sh
+++ build_test_stereo.sh
@@ -7,5 +7,5 @@ then
     outbits=24
 fi
 
-g++ *.c *.cpp -std=c++17 -O3 -o dsd2dxd
+g++ *.c *.cpp -std=c++17 -O2 -o dsd2dxd
 ./dsd2dxd -f $1 -b $2 -e $3 --volume=$4 < $5 | ffplay -f s${outbits}le -ar 352.8k -ac 2 -i -
