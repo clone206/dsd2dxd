28c5497c17e9631582d27605ba1755fa56b5870a
clone206 <clone206@example.com>
2024-08-17 18:23:42 -0700

    Fixes to test scripts

--- build_test_stereo_flt.sh
+++ build_test_stereo_flt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 g++ *.c *.cpp -std=c++17 -O2 -o dsd2dxd $(pkg-config --libs --cflags taglib flac++)
-./dsd2dxd -f $1 -b 32 -d F -e $2 --volume=$3 < $4 | ffplay -f f32le -ar 352.8k -ac 2 -i -
+./dsd2dxd -f $1 -b 32 -d F -e $2 --level=$3 < $4 | ffplay -f f32le -ar 352.8k -ac 2 -i -
