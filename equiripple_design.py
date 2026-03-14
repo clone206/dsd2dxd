619ae3bee140eaf430e1effca731260b4f60bd30
clone206 <clone206@local>
2026-03-13 17:00:59 -0700

    updated equiripple py file

--- equiripple_design.py
+++ equiripple_design.py
@@ -6,8 +6,8 @@ from filter_lib import *
 
 parser = argparse.ArgumentParser(description="Design an equiripple FIR filter and emit Rust taps")
 parser.add_argument("--fs", type=float, required=True, help="Sampling rate in Hz")
-parser.add_argument("--fsb", type=float, required=True, help="Stopband edge in Hz")
 parser.add_argument("--fpb", type=float, required=True, help="Passband edge in Hz")
+parser.add_argument("--fsb", type=float, required=True, help="Stopband edge in Hz")
 args = parser.parse_args()
 
 Fs      = args.fs
@@ -21,6 +21,13 @@ N = fir_find_optimal_N(Fs, Fpb, Fsb, Apb, Asb)
 (h, w, H, Rpb, Rsb, Hpb_min, Hpb_max, Hsb_max) =  fir_calc_filter(Fs, Fpb, Fsb, Apb, Asb, N)
 
 ht_len = len(h)//2
+print('/*')
+print(f' * Fs: {Fs} Hz')
+print(f' * Fpb: {Fpb} Hz')
+print(f' * Fsb: {Fsb} Hz')
+print(f' * Apb: {Apb} dB')
+print(f' * Asb: {Asb} dB')
+print('*/')
 print(f'pub const HTAPS_EQ: [f64; {ht_len}] = [')
 print(*h[ht_len:], sep=',\n')
 print('];')
